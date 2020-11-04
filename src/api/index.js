a48d5feca3677b19b1b4d00648bbc04d8dc777e1
allimacse <alan.escamillam@gmail.com>
2020-11-03 21:12:16 -0600

    feat: clase11

--- src/api/index.js
+++ src/api/index.js
@@ -1,14 +1,18 @@
 const express = require('express');
+const cors = require('cors');
+const fs = require('fs');
 const morgan = require('morgan');
 const path = require('path');
 const celebrate = require('celebrate');
-const { logDate } = require('../middleware/index.js');
+const { logDate, errorHandler } = require('../middleware/index.js');
 
 const app = express();
 const PORT = process.env.PORT || 4000;
 
+const accessLogStream = fs.createWriteStream(path.join(__dirname, '../../access.log'), { flags: 'a' });
+app.use(morgan('combined', { stream: accessLogStream }));
+app.use(cors());
 app.use(logDate);
-app.use(morgan('dev'));
 app.use(express.json({ extended: true }));
 
 app.get('/', (req, res) => {
@@ -18,6 +22,8 @@ app.get('/', (req, res) => {
 app.use('/api/v1', require('../routers/index.js'));
 
 app.use(celebrate.errors());
+
+app.use(errorHandler);
 
 module.exports = {
   app,
